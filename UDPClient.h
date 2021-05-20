b3d16f0df58d663226c9d0a84fc3705a5839d060
Waldemar <waldek99zielinski@gmail.com>
2021-05-20 15:07:14 +0200

    UDP: server

--- UDPClient.h
+++ UDPClient.h
@@ -22,17 +22,18 @@ public:
         const char *message = data;
 
         //create udp socket
-        int fileDesc = socket(AF_INET, SOCK_DGRAM, 0);
-        std::cout << "filedesc " << fileDesc << std::endl;
+        int socketDesc = socket(AF_INET, SOCK_DGRAM, 0);
+        std::cout << "socketDesc " << socketDesc << std::endl;
         //TODO: catch exceptions :)
-        if (fileDesc < 0)
+        if (socketDesc < 0)
         {
-            std::cout << "fileDesc" << std::endl;
+            std::cout << "socketDesc" << std::endl;
             perror("sock error");
         }
 
+        //set socket options to broadcast
         int optval = 1;
-        int options = setsockopt(fileDesc, SOL_SOCKET, SO_BROADCAST, (char *)&optval, sizeof(optval));
+        int options = setsockopt(socketDesc, SOL_SOCKET, SO_BROADCAST, (char *)&optval, sizeof(optval));
 
         if (options == -1)
         {
@@ -48,7 +49,7 @@ public:
         addr.sin_port = htons(10000);
 
         //sending data
-        int feed = sendto(fileDesc, message, strlen(message), 0, (struct sockaddr *)&addr, sizeof(addr));
+        int feed = sendto(socketDesc, message, strlen(message), 0, (struct sockaddr *)&addr, sizeof(addr));
 
         if (feed < 0)
         {
@@ -58,6 +59,52 @@ public:
     }
     void serwer()
     {
+
+        //create udp socket
+        int socketDesc = socket(AF_INET, SOCK_DGRAM, 0);
+        if (socketDesc < 0)
+        {
+            std::cout << "socketDesc" << std::endl;
+            perror("sock error");
+        }
+        struct sockaddr_in addr;
+        memset(&addr, 0, sizeof(struct sockaddr_in));
+        addr.sin_family = AF_INET;
+        // addr.sin_addr.s_addr = inet_addr("192.168.0.255");
+        addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        addr.sin_port = htons(10000);
+
+        //bind socket to address
+        int bindFeed = bind(socketDesc, (struct sockaddr *)&addr, sizeof(addr));
+        if (bindFeed < 0)
+        {
+            perror("bind error");
+            exit(1);
+        }
+        while (1)
+        {
+            char messageBuffer[1024];
+            fd_set readfds;
+            struct sockaddr_in clientAddr;
+            socklen_t addrLen = sizeof(clientAddr);
+            FD_ZERO(&readfds);
+            FD_SET(socketDesc, &readfds);
+
+            int selectFeed = select(socketDesc + 1, &readfds, NULL, NULL, 0);
+            //number of fdesc actions
+            if (selectFeed > 0)
+            {
+                if (FD_ISSET(socketDesc, &readfds))
+                {
+                    int receivedBytes = recvfrom(socketDesc, messageBuffer, sizeof(messageBuffer), 0, (struct sockaddr *)&clientAddr, &addrLen);
+
+                    if (receivedBytes > 0)
+                    {
+                        std::cout << messageBuffer << std::endl;
+                    }
+                }
+            }
+        }
     }
 };
 
