bce1c253f6d4db86948c75c88bad5b4bffe85241
KrissKry <edmaister02@gmail.com>
2021-06-08 14:25:31 +0200

    get ip from socket implemented

--- include/NetworkHandler.h
+++ include/NetworkHandler.h
@@ -93,6 +93,19 @@ private:
     *   @return file descriptor id on success, -1 on error 
     */
     int openNewSocket();
+
+
+    std::string getIPFromSocket(int sockfd) {
+        struct sockaddr_in addr;
+        socklen_t addr_size = sizeof(addr);
+
+        int res = getpeername(sockfd, (struct sockaddr *)&addr, &addr_size);
+
+        std::string ip = inet_ntoa(addr.sin_addr);
+        // std::cout << "[WOOOO] " <<  ip << "\n";
+
+        return ip;
+    }
 };
 
 #endif
